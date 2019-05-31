307de8e5f90e4b399c86e8f38775d0136d241eee
Akash Aman <akashaman679@gmail.com>
2019-06-01 01:07:27 +0530

    DB_Script
    Database Script File

---
+++ DbScript.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[movies] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [Title]        NVARCHAR (100) NOT NULL,
+    [Director]     NVARCHAR (100) NOT NULL,
+    [DateReleased] DATE       NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
